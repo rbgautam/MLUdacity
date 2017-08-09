1b7c46c18bdff2d45caa9c1033739d12a42946e4
CORPORATE\RGautam <forgeinnovations@gmail.com>
2017-08-09 13:52:16 -0500

    numpy demo

---
+++ numpy_demo.py
@@ -0,0 +1,73 @@
+# Use the numpy library
+import numpy as np
+
+
+def prepare_inputs(inputs):
+    # TODO: create a 2-dimensional ndarray from the given 1-dimensional list;
+    #       assign it to input_array
+    input_array = np.array([inputs])
+    
+    # TODO: find the minimum value in input_array and subtract that
+    #       value from all the elements of input_array. Store the
+    #       result in inputs_minus_min
+    arrmin = np.amin(input_array)
+    inputs_minus_min = input_array - arrmin
+
+    # TODO: find the maximum value in inputs_minus_min and divide
+    #       all of the values in inputs_minus_min by the maximum value.
+    #       Store the results in inputs_div_max.
+    arrmax = np.amax(input_array)
+    inputs_div_max = input_array/arrmax
+
+    # return the three arrays we've created
+    return input_array, inputs_minus_min, inputs_div_max
+    
+
+def multiply_inputs(m1, m2):
+    # TODO: Check the shapes of the matrices m1 and m2. 
+    #       m1 and m2 will be ndarray objects.
+    #
+    m1shp0 = m1.shape[0] 
+    m2shp1 = m2.shape[1]
+
+    m1shp1 = m1.shape[1] 
+    m2shp0 = m2.shape[0]
+
+
+    #       Return False if the shapes cannot be used for matrix
+    #       multiplication. You may not use a transpose
+    if m1.shape[0] != m2.shape[1] and m1.shape[1] != m2.shape[0] :
+        return False
+    pass
+
+
+    # TODO: If you have not returned False, then calculate the matrix product
+    #       of m1 and m2 and return it. Do not use a transpose,
+    #       but you swap their order if necessary
+    if m1.shape[1] == m2.shape[0]:
+        return np.matmul(m1, m2)
+    else:
+        return np.matmul(m2, m1)
+    pass
+    
+
+def find_mean(values):
+    # TODO: Return the average of the values in the given Python list
+    totval = 0
+    size = len(values) 
+    for i in range(size):
+        totval += values[i]
+    return totval/size    
+    pass
+
+
+input_array, inputs_minus_min, inputs_div_max = prepare_inputs([-1,2,7])
+print("Input as Array: {}".format(input_array))
+print("Input minus min: {}".format(inputs_minus_min))
+print("Input  Array: {}".format(inputs_div_max))
+
+print("Multiply 1:\n{}".format(multiply_inputs(np.array([[1,2,3],[4,5,6]]), np.array([[1],[2],[3],[4]]))))
+print("Multiply 2:\n{}".format(multiply_inputs(np.array([[1,2,3],[4,5,6]]), np.array([[1],[2],[3]]))))
+print("Multiply 3:\n{}".format(multiply_inputs(np.array([[1,2,3],[4,5,6]]), np.array([[1,2]]))))
+
+print("Mean == {}".format(find_mean([1,3,4])))
